8ad97e26598cfe861e40d8ef23e78b8a9733eb66
anniebae <bae.annie@gmail.com>
2016-05-14 15:01:11 -0400

    binding to receive data from the appComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 
 export class Hero {
   id: number;
@@ -18,14 +20,7 @@ export class Hero {
       </li>
     </ul>
 
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name">
-      </div>
-    </div>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
   styles:[`
     .selected {
